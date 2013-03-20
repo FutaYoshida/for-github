ba689b28be7f69099d42a2649b3ac885b55e6f69
FutaYoshida <magicutan853@i.softbank.jp>
2013-03-20 23:41:56 +0900

    コードリファクタリング　フェイズ２
    生成クラス、アイコン系クラスの圧縮

--- main/ver1.4/main3.js
+++ main/ver1.4/main3.js
@@ -3,16 +3,22 @@ window.onload= function(){
 	var core = new Core(640,480);
 	core.fps=15;
 	core.preload('char.gif','char2.gif','kgen.png');
-	core.preload('layout.jpg');
+	core.preload('layout.jpg','bearteam.jpg','blik.jpg');
 
 	core.onload=function(){
-		var fri=[];
-		var ene=[];
+		//生成キャラクラス
 		var FriGen= Class.create(Sprite,{
 			initialize:function(x,y){
 				ClassimgCreate(this,50,40,"kgen.png",core);
 				this.x=x;
 				this.y=y;
+				//出撃ゲージ
+				this.gauge=new Images(x+60,y,10,40,"",core);
+				this.gauge.backgroundColor="#f00";	
+				this.gauge2=new Images(x+60,y,10,40,"",core);
+				this.gauge2.backgroundColor="#00F";
+				this.gauge2.on('enterframe',function(){
+					(this.height>0)?this.height-=1:this.height=40;});
 			}
 		});
 		var EneGen= Class.create(Sprite,{
@@ -20,6 +26,14 @@ window.onload= function(){
 				ClassimgCreate(this,50,40,"kgen.png",core);
 				this.x=x;
 				this.y=y;
+				//出撃ゲージ
+				this.gauge=new Images(x+60,y,10,40,"",core);
+				this.gauge.backgroundColor="#f00";	
+				this.gauge2=new Images(x+60,y,10,40,"",core);
+				this.gauge2.backgroundColor="#00F";
+				this.gauge2.on('enterframe',function(){
+					(this.height>0)?this.height-=1:this.height=40;});
+
 			}
 		});
 		//一般イメージ用クラス（引数全部入り）
@@ -30,16 +44,28 @@ window.onload= function(){
 				this.y=y;
 			}
 		});
-
+		//スプライトは下の層から順に書くこと
 		var bg = new Images(0,0,640,480,"layout.jpg");	
+		var friendteam = new Images(10,10,140,50,"bearteam.jpg");
+		var enemyteam = new Images(490,10,140,50,"blik.jpg");
+		var enemygoal = new Images(80,120,10,270,"");
+		var friendgoal = new Images(560,120,10,270,"");
+		//仮の色付け
+		enemygoal.backgroundColor="ee00ee";
+		friendgoal.backgroundColor="ee00ee";
+		//キャラクタ生成クラスは一番上の層に
+		var fri=[];
+		var ene=[];
+		
 		for(i=0;i<5;i++){
 			fri[i]=new FriGen(10,130+(50*i));
 			ene[i]=new EneGen(570,130+(50*i));
 		}
-			
+
 	};
 	core.start();
 };
+//クラス生成を一括の関数化(this,幅、高さ、画像ファイル名、core)として使用
 function ClassimgCreate(e,w,h,img,Corename){
 	Sprite.call(e,w,h);
 	e.image=Corename.assets[img];
