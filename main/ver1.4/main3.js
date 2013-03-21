4d9790e8513689b113b51e1e26ada66ec3b68a01
FutaYoshida <magicutan853@i.softbank.jp>
2013-03-21 21:51:30 +0900

    コードリファクタリング　フェイズ３
    画像表示に軽度バグ有り。

--- main/ver1.4/main3.js
+++ main/ver1.4/main3.js
@@ -1,11 +1,18 @@
 enchant();
 window.onload= function(){
 	var core = new Core(640,480);
-	core.fps=15;
+	core.fps=10;
 	core.preload('char.gif','char2.gif','kgen.png');
-	core.preload('layout.jpg','bearteam.jpg','blik.jpg');
+	core.preload('layout.jpg','bearteam.jpg','blik.jpg','flag.jpg');
 
 	core.onload=function(){
+		
+	var friendcount=0;
+	var enemycount=0;
+	var friendset=0;
+	var enemyset=0;
+	var Charas=[[],[]]
+var friendCharaNo=0;
 		//生成キャラクラス
 		var FriGen= Class.create(Sprite,{
 			initialize:function(x,y){
@@ -19,6 +26,19 @@ window.onload= function(){
 				this.gauge2.backgroundColor="#00F";
 				this.gauge2.on('enterframe',function(){
 					(this.height>0)?this.height-=1:this.height=40;});
+			
+				   this.on('touchstart',function(){
+					   if(friendcount<5){
+						   for(var i=0; i<=5; i++){
+							   if(Charas[0][i]==undefined){
+								   friendCharaNo=i;
+								   Charas[0][i]=new FriendChara(90,220);
+								   friendcount++;
+								   break;
+							   }
+						   }
+					   }
+				   });
 			}
 		});
 		var EneGen= Class.create(Sprite,{
@@ -36,6 +56,21 @@ window.onload= function(){
 
 			}
 		});
+		//登場キャラクラス
+		var FriendChara =Class.create(Sprite,{
+			initialize:function(x,y){
+				ClassimgCreate(this,64,64,"char.gif",core);
+				this.x=x;
+				this.y=y;
+				this.frame=[0,1,0,2];
+				CharaParam(this,0);
+				this.on('enterframe',function(){
+					Charamove(this,core);
+				});
+			}
+
+		});
+
 		//一般イメージ用クラス（引数全部入り）
 		var Images= Class.create(Sprite,{
 			initialize:function(x,y,w,h,imgname){
@@ -50,6 +85,22 @@ window.onload= function(){
 		var enemyteam = new Images(490,10,140,50,"blik.jpg");
 		var enemygoal = new Images(80,120,10,270,"");
 		var friendgoal = new Images(560,120,10,270,"");
+		var friendflag = [];
+		var enemyflag =[];
+		for(var i=0;i<friendcount;i++){
+			if(i<5){
+			  friendflag[i]=new Images(120+(i*30),70,20,20,"flag.jpg");}
+		
+		else if(i<10){friendflag[i]=new Images((i*30)-30,90,20,20,"flag.jpg");}
+		}
+
+		for(var i=0;i<enemycount;i++){
+			if(i<5){
+			  enemyflag[i]=new Images(380+(i*30),70,20,20,"flag.jpg");}
+		
+		else if(i<10){enemyflag[i]=new Images((i*30)+230,90,20,20,"flag.jpg");}
+		}
+
 		//仮の色付け
 		enemygoal.backgroundColor="ee00ee";
 		friendgoal.backgroundColor="ee00ee";
@@ -70,4 +121,12 @@ function ClassimgCreate(e,w,h,img,Corename){
 	Sprite.call(e,w,h);
 	e.image=Corename.assets[img];
 	Corename.rootScene.addChild(e);
+}
+
+function CharaParam(e,Charano){
+ var Spd=[10,20,5,10,10,5,-10,-20,-5,-10,-10];
+ e.spd=Spd[Charano];
+}
+function Charamove(e,Corename){
+	e.x+=e.spd/Corename.fps;
 }
