e233f875c80df51a2154b9ad7d4e69bc00493058
FutaYoshida <magicutan853@i.softbank.jp>
2013-03-31 22:52:04 +0900

    #2 SE アイコン無し初期バージョン完成

--- main/ver2.0/game.js
+++ main/ver2.0/game.js
@@ -1,14 +1,24 @@
 enchant();
 window.onload= function(){
 	var core = new Core(640,480);
-	core.preload('title.jpg','layout.jpg','bearteam.jpg','blik.jpg','waiticon.gif','char.gif');
+	core.preload('title.jpg','layout02.jpg','bearteam.jpg','blik.jpg','waiticon.gif','char.gif','char2.gif','flag.jpg','round.jpg');
 	core.fps=5;
 	core.onload=function(){
 		/*シーン設定*/
 		var Title= new Scene();
 		var game =new Scene();
-		enemies= new Array();
-		friends= new Array();
+		var result=new Scene();
+		var enemies= new Array();
+		var friends= new Array();
+	var friendflag= new Array();
+	var enemyflag= new Array();
+	var friendstar= new Array();
+	var enemystar= new Array();
+	var time =60;
+	var friendcount=0;
+	var enemycount=0;
+	var friendround=0;
+	var enemyround=0;
 		/*クラス設定*/ 
 		var Generater=Class.create(Sprite,{
 			initialize:function(x,y){
@@ -36,16 +46,20 @@ window.onload= function(){
 		var FriGen=Class.create(Generater,{
 			initialize:function(x,y){
 				Generater.call(this,x,y);
-	this.on('touchmove',function(e){
-					this.marker.y=e.y;
-				if(e.x<300)this.marker.x=e.x;
+				this.on('touchmove',function(e){
+					if(e.y<220){this.marker.y=130;}
+					else if(e.y<310){this.marker.y=220;}
+					else{this.marker.y=310;}
+					if(e.x<200&&e.x>100)this.marker.x=e.x;
+					else{this.marker.x=101;}
 				});
 
 				this.on('touchend',function(){
-					if(this.counter>=40){this.counter=0;	this.gauge2.height=0;
+					if(this.counter>=40){
 						for(i=0;i<5;i++){
 							if(friends[i]==undefined){
 								friends[i]=new Funit(this.marker.x,this.marker.y,i);
+								this.counter=0;	this.gauge2.height=0;
 								break;}
 						}
 					}
@@ -58,6 +72,16 @@ window.onload= function(){
 			initialize:function(x,y){
 				Generater.call(this,x,y);
 				this.frame=1;
+				this.on('enterframe',function(){
+					if(this.counter>=40&&this.age%(50-rand(10))==0){
+						for(i=0;i<4;i++){
+							if(enemies[i]==undefined){
+								enemies[i]=new Eunit(496,130+(90*rand(2)),i);
+								this.counter=0;this.gauge2.height=0;
+								break;}
+						}
+					}
+				});
 			}
 		});
 		var Unit=Class.create(Sprite,{
@@ -73,10 +97,10 @@ window.onload= function(){
 		var Funit=Class.create(Unit,{
 			initialize:function(x,y,no){
 				Unit.call(this,x,y,0);
-				console.log(this.image);
 				this.no=no;
+				this.hp=30;
 				this.on('enterframe',function(){
-					this.x+=1;
+					this.x+=5;
 					for(var i in enemies){
 						if(enemies[i].within(this,10)){
 						enemies[i].crush();	
@@ -95,8 +119,9 @@ window.onload= function(){
 		    },
 		    crush:function(){
 			    this.frame=3;
-			    this.tl.moveBy(-200,0,4).then(function(){if(this.frame==3){this.frame=[0,1,0,2];}
-				    else{this.fdout();}
+			    this.hp-=15;
+			    this.tl.moveBy(-200,0,4).then(function(){if(this.hp<=0){this.fdout();}
+				    else{this.frame=[0,1,0,2]}
 			    });
 			    
 		    },
@@ -107,13 +132,14 @@ window.onload= function(){
 		});
 		var Eunit=Class.create(Unit,{
 			initialize:function(x,y,no){
-				Unit.call(this,x,y,0);
-				this.scaleX=-1;
-				this.no=no
+				Unit.call(this,x,y,1);
+				this.no=no;
+				this.hp=30;
 			this.on('touchstart',function(){
 				this.remove();
 			});
-
+this.on('enterframe',function(){
+this.x-=5;});
 			},
 		    remove:function(){
 			    game.removeChild(this);
@@ -121,7 +147,11 @@ window.onload= function(){
 		    },
 		    crush:function(){
 			    this.frame=3;
-			    this.tl.moveBy(200,0,4).then(function(){this.fdout();});
+			    this.hp-=15;
+			    this.tl.moveBy(200,0,4).then(function(){if(this.hp<=0){this.fdout();}
+				    else{this.frame=[0,1,0,2];}
+			    }
+				    );
 		    },
 		    fdout:function(){
 			    this.tl.fadeOut(5).then(function(){this.remove();});
@@ -138,6 +168,18 @@ window.onload= function(){
 				this.y=y;
 			}
 		});
+		var Flag= Class.create(Images,{
+			initialize:function(x,y,no){
+				Images.call(this,x,y,20,20,"flag.jpg");
+				this.no=no;
+			}
+		});
+		var Round= Class.create(Images,{
+			initialize:function(x,y,no){
+				Images.call(this,x,y,30,30,"round.jpg");
+				this.no=no;
+			}
+		});	
 		/*タイトル画面の構成物*/
 		var titleG=new Images(0,0,640,480,"title.jpg");
 		titleG.on('touchstart',function(){
@@ -149,31 +191,138 @@ window.onload= function(){
 		titlelabel.x=320;
 		titlelabel.y=450;
 		addC(titlelabel,Title);
+		/*ポーズ画面の構成物*/
+		var Pose=new Images(160,120,320,240,"");
+		Pose.backgroundColor="cccccc";
+		Pose.on('touchstart',function(){
+			core.popScene(result);
+		});
+		addC(Pose,result);
 		/*ゲーム画面の構成物*/
-		var Bg=new Images(0,0,640,480,"layout.jpg");
+		var Bg=new Images(0,0,640,480,"layout02.jpg");
 		addC(Bg,game);
 		var team=[new Images(10,10,140,50,"bearteam.jpg"),new Images(490,10,140,50,"blik.jpg")];
 		for(i in team){	addC(team[i],game);}
+		team[0].on('enterframe',function(){
+			for(i=0;i<friendcount;i++){
+				if(friendflag[i]==undefined){
+					if(i<5){
+						friendflag[i]=new Flag(120+(i*30),70,i);addC(friendflag[i],game);}
+					else if(i<10){
+						friendflag[i]=new Flag((i*30)-30,90,i);addC(friendflag[i],game);}
+					else{core.pushScene(result);
+						//timer.counter=0;
+						for(i in friends){
+							friends[i].remove();
+						}
+						for(i in enemies){
+							enemies[i].remove();}
+						friendcount=0;
+						enemycount=0;
+						for(i in friendflag){
+							game.removeChild(friendflag[i]);
+							delete friendflag[i];
+						}
+						for(i in enemyflag){
+							game.removeChild(enemyflag[i]);
+							delete enemyflag[i];
+					
+						}
+						friendround++;
+					}
+
+				}
+			}
+			for(i=0;i<friendround;i++){
+				if(friendstar[i]==undefined){
+					friendstar[i]=new Images(20+(40*i),70,30,30,"round.jpg");
+					addC(friendstar[i],game);
+				}
+				if(friendround==2){core.end();}
+			}
+
+		});
+
+		team[1].on('enterframe',function(){
+			for(i=0;i<enemycount;i++){
+				if(enemyflag[i]==undefined){
+					if(i<5){
+						enemyflag[i]=new Flag(380+(i*30),70,i);addC(enemyflag[i],game);}
+					else if(i<10){
+						enemyflag[i]=new Flag((i*30)+230,90,i);addC(enemyflag[i],game);}
+					else{core.pushScene(result);
+				//		timer.counter=0;
+						for(i in friends){
+							friends[i].remove();
+						}
+						for(i in enemies){
+							enemies[i].remove();}
+						friendcount=0;
+						enemycount=0;
+						for(i in friendflag){
+							game.removeChild(friendflag[i]);
+							delete friendflag[i];
+						}
+						for(i in enemyflag){
+							game.removeChild(enemyflag[i]);
+							delete enemyflag[i];
+						}
+						for(i in ene){
+							ene[i].counter=40;
+							ene[i].gauge2.height=40;
+						}
+						enemyround++;
+					}	
+				}
+			}
+			for(i=0;i<enemyround;i++){
+				if(enemystar[i]==undefined){
+					enemystar[i]=new Images(550+(40*i),70,30,30,"round.jpg");
+					addC(enemystar[i],game);
+				}
+				if(enemyround==2){core.end();}
+			}
+
+		});
+		
+
 		var fri=[];
 		var ene=[];
 		for(i=0;i<5;i++){
 			fri[i]=new FriGen(10,130+(50*i)); addC(fri[i],game);
 			ene[i]=new EneGen(570,130+(50*i));addC(ene[i],game);
-
 		}
-		//var c1=new Funit(100,100);
-		//var enemy= new Eunit(200,100,0);
-		//var enemy2= new Eunit(300,100,1);
-		//enemies[0]=enemy;
-		//enemies[1]=enemy2;
-		for(i=0;i<5;i++){
-		  var enemy=new Eunit(100*(1+i),130+(50),i);
-		  enemies[i]=enemy;
-		  }
+		var friendgoal=new Images(560,120,10,270,"");addC(friendgoal,game);
+		friendgoal.backgroundColor="ee00ee";
+		friendgoal.on('enterframe',function(){
+			for(var i in friends){
+				if(this.intersect(friends[i])){
+					friends[i].remove();
+					friendcount++;
+				}
+			}
+		});
+var enemygoal=new Images(80,120,10,270,"");addC(enemygoal,game);
+		enemygoal.backgroundColor="ee00ee";
+		enemygoal.on('enterframe',function(){
+			for(var i in enemies){
+				if(this.intersect(enemies[i])){
+					enemies[i].remove();
+					enemycount++;
+				}
+			}
+		});
 
-
-
-
+/*var timer= new Label();
+timer.text="Time";
+timer.x=300;
+timer.y=20;
+timer.counter=0;
+timer.on('enterframe',function(){
+	this.tl.delay(core.fps).then(function(){this.counter+=1});
+	this.text=time-this.counter;
+});
+addC(timer,game);*/
 		//ゲーム開始時はタイトル画面を出す
 		core.pushScene(Title);
 	};
@@ -187,3 +336,5 @@ function ClassImg(e,w,h,img,Coren){
 function addC(e,Scenename){
 	Scenename.addChild(e);
 }
+function  rand(n){
+	return Math.floor(Math.random()*(n+1));}
